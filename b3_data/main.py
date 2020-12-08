e1605113ab24f9fde82233689a53ab13c5d0d736
Wesley Batista <wesley.batista.santos@gmail.com>
2020-12-07 21:17:25 -0300

    add upload command

--- b3_data/main.py
+++ b3_data/main.py
@@ -2,7 +2,10 @@ from datetime import date
 import click
 
 
-@click.group()
+PREFIX = 'B3DATA'
+
+
+@click.group(context_settings=dict(auto_envvar_prefix=PREFIX))
 def cli():
     "Utility for http://b3.com.br datasets"
 
@@ -16,3 +19,15 @@ def download(date, chunk_size):
     """Downloads quotes data"""
     from b3_data import download
     download.download_tickercsv(str(date.date()), chunk_size)
+
+
+@cli.command()
+@click.argument("date",
+                type=click.DateTime(formats=["%Y-%m-%d"]),
+                default=str(date.today()))
+@click.option("--chunk-size", type=int, default=100000)
+@click.option('--dsn', required=True, help=f'Set the environment variable {PREFIX}_UPLOAD_DSN')
+def upload(date, chunk_size, dsn):
+    """Uploads quotes data"""
+    from b3_data import upload
+    upload.upload(dsn, chunk_size, str(date.date()))
