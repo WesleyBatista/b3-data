81ed41a52f0b4f6130ce313de9ed0bf021dfbd3f
Wesley Batista <wesley.batista.santos@gmail.com>
2021-02-17 18:17:33 +0100

    fix date formating

--- b3_data/main.py
+++ b3_data/main.py
@@ -12,12 +12,11 @@ def cli():
 @click.option("--chunk-size", type=int, default=10000)
 def download(date, chunk_size):
     """Downloads quotes data"""
-    print(date)
     if not date:
         import subprocess
         date = subprocess.getoutput("""echo $(curl --silent 'https://arquivos.b3.com.br/apinegocios/dates') | sed -e 's/"//g' -e 's/\[//g' -e 's/\]//g' | cut -d"," -f 1""")
     else:
-        date = str(date)
+        date = str(date.date())
 
     from b3_data import download
     download.download_tickercsv(date, chunk_size)
